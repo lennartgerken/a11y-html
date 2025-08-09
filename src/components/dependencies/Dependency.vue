4214e412a7a090a66df2c912bce4329ea919d484
lennartgerken <49632312+lennartgerken@users.noreply.github.com>
2025-08-09 18:04:44 +0200

    Add mit text to Dependencies.vue

--- src/components/dependencies/Dependency.vue
+++ src/components/dependencies/Dependency.vue
@@ -4,20 +4,16 @@ const props = defineProps<{
     copyright?: string,
     usage?: string,
     repo: string,
-    license: string,
-    licenceUrl: string
 }>()
 </script>
 
 <template>
     <div>
-        <div class="text-2xl">{{ title }}</div>
+        <div class="text-xl">{{ title }}</div>
         <div v-if="copyright">{{ copyright }}</div>
         <div class="font-bold" v-if="usage">Usage:</div>
         <div v-if="usage">{{ usage }}</div>
         <div class="font-bold">Repository:</div>
         <a :href="repo" target="_blank">{{ repo }}</a>
-        <div class="font-bold">License:</div>
-        <div>{{ license }} (<a :href="licenceUrl" target="_blank">{{ licenceUrl }}</a>)</div>
     </div>
 </template>
