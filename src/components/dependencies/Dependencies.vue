efc9bab9fe2e668289d893b6d63ab847ef971143
lennartgerken <49632312+lennartgerken@users.noreply.github.com>
2025-08-09 09:03:29 +0200

    add dependencies to report

---
+++ src/components/dependencies/Dependencies.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Dependency from './Dependency.vue';
+</script>
+
+<template>
+    <div>
+        <div class="text-3xl">Dependencies</div>
+        <div class="mt-5 flex flex-col gap-5">
+            <Dependency title="a11y-html" copyright="Copyright (c) 2025 Lennart Gerken"
+                repo="https://github.com/lennartgerken/a11y-html" license="MIT"
+                licenceUrl="https://opensource.org/license/mit" />
+            <Dependency title="axe-core" details="Used for type imports." repo="https://github.com/dequelabs/axe-core"
+                license="MPL-2.0" licenceUrl="https://www.mozilla.org/en-US/MPL/2.0/" />
+            <Dependency title="tailwindcss" copyright="Copyright (c) Tailwind Labs, Inc."
+                repo="https://github.com/tailwindlabs/tailwindcss" license="MIT"
+                licenceUrl="https://opensource.org/license/mit" />
+            <Dependency title="vue" copyright="Copyright (c) 2018-present, Yuxi (Evan) You"
+                repo="https://github.com/vuejs/core" license="MIT" licenceUrl="https://opensource.org/license/mit" />
+        </div>
+
+    </div>
+</template>
