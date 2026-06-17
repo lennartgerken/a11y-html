aa02365d6c513714e1fc9385466f840eaf336194
Lennart Gerken <49632312+lennartgerken@users.noreply.github.com>
2026-06-17 08:24:14 +0000

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
@@ -14,7 +13,7 @@ export default defineConfig({
             external: ['axe-core']
         }
     },
-    plugins: [vue(), vueDevTools(), tailwindcss(), viteSingleFile()],
+    plugins: [vue(), tailwindcss(), viteSingleFile()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
